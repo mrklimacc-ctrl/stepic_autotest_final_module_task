e01e0cd452a9fc11d70ca61c1f33da93f93a4d4d
Klim <mr.klimacc@gmail.com>
2026-06-25 16:09:57 +0300

    Поменяли логику переходов
    Вынесли создание объектов новых страниц в тело теста

--- pages/locators.py
+++ pages/locators.py
@@ -4,5 +4,5 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    REGISTRATION_FORM = (By.ID, "#register_form")
-    LOGIN_FORM = (By.ID, "#login_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,14 @@
 
 from selenium.webdriver.remote.webdriver import WebDriver
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 # pytest -v --tb=line --language=en test_main_page.py
 
 def test_guest_can_go_to_login_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    page.should_be_login_link()      # проверяем, что есть ссылка на логин
+    main_page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    main_page.open()                      # открываем страницу       
+    main_page.go_to_login_page() # переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)  # Создаем объект LoginPage с url текущей страницы после перехода по ссылке
+    login_page.should_be_login_page()  # проверяем, что оказались на странице логина
