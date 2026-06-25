e169b59234ca078c5d26cf85a74fd9ccbfd59db6
Klim <mr.klimacc@gmail.com>
2026-06-25 15:07:41 +0300

    Добавление функции поиска элемента на странице
    Добавил возможность проверки присутствия объекта на странице в BasePage
    Добавил проверку присутствия кнопки логина на странице в MainPage
    Добавил тест на проверку присутствия кнопки логина на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,23 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from typing import Literal
+
+# Ограничиваем тип только теми строками, которые есть в By
+ByLocatorStrategy = Literal["css selector", "xpath", "id", "name", "link text", "partial link text", "tag name", "class name"]
 
 class BasePage:
-    def __init__(self, browser: WebDriver, url: str):
+    def __init__(self, browser: WebDriver, url: str, timeout: int = 10):
         self.browser = browser
         self.url = url
+        browser.implicitly_wait(timeout)  # Устанавливаем неявное ожидание в timeout секунд
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how: ByLocatorStrategy, what: str) -> bool:
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
